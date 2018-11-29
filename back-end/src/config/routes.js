9036b719d8489746a15693d285b099ad9dab1fba
rcalegario <rodrigocalegario@gmail.com>
2018-11-29 19:31:57 -0300

    Atulizando configuração do cors

--- back-end/src/config/routes.js
+++ back-end/src/config/routes.js
@@ -1,8 +1,17 @@
-const cors = require("../common/security/cors.security");
+// const corsOptions = require("../common/security/cors.security");
+const cors = require('cors')
 const diskRoutes = require('../resource/disk/disk.routing');
 
 module.exports = (app) => {
-  app.use(cors);
+  const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  // Cors Middlewares
+  app.use(cors(corsOptions));
   app.get("/", (req, res) => res.redirect("/api-docs"));
   app.use("/api/disks", diskRoutes);
 };
