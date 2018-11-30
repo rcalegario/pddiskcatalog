cb23cf4f9ef85b338000ed0107219ef373f85e83
rcalegario <rodrigocalegario@gmail.com>
2018-11-29 21:44:35 -0300

    Modificação na configutação do database

--- back-end/src/config/database.js
+++ back-end/src/config/database.js
@@ -2,10 +2,10 @@ const  mysql = require('mysql');
 const util = require('util')
 
 const pool = mysql.createPool({ 
-  host : 'localhost',
-  user : 'root',
-  password : '',
-  database : 'pd-disk-catalog'
+  host : process.env.DB_HOST,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE
  });
 
  pool.getConnection((err, connection) => {
